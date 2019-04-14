15adc18ce12a7f55e02845573d9f48ebc901f81c
jabrihamza <jabrihamza1996@gmail.com>
2019-04-14 20:12:33 +0100

    add firebase configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBckbNDdU2oIzvtmE77Rb6ews2Qcyc0iYQ',
+    authDomain: 'yumii-live-chat.firebaseapp.com',
+    databaseURL: 'https://yumii-live-chat.firebaseio.com',
+    projectId: 'yumii-live-chat',
+    storageBucket: 'yumii-live-chat.appspot.com',
+    messagingSenderId: '205858596099'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBckbNDdU2oIzvtmE77Rb6ews2Qcyc0iYQ',
+    authDomain: 'yumii-live-chat.firebaseapp.com',
+    databaseURL: 'https://yumii-live-chat.firebaseio.com',
+    projectId: 'yumii-live-chat',
+    storageBucket: 'yumii-live-chat.appspot.com',
+    messagingSenderId: '205858596099'
+  }
 };
 
 /*
